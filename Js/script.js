f7289913378879d9aa3f3941d77026de61d88f2a
snir2808 <sniroos@gmail.com>
2021-03-09 16:04:34 +0200

    adddd

--- Js/script.js
+++ Js/script.js
@@ -95,12 +95,14 @@ $(document).ready(function(){
 
     })
 
-    $("#navigation li a").click(function(e){
-        e.preventDefault();
-        var targetElement = $(this).attr("href");
-        var targetPosition = $(targetElement).offset().top;
-        $("html, body").animation({scrollTop: targetPosition - 50 }, 'slow')
-    })
+    $("#navigation li a").click(function(e) {
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+
+	});
 
 
     const nav = $("#navigation")
